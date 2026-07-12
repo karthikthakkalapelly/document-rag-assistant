cc434a93a16af5b7a6432dc07c2d3ef20369151f
tk-hub-7 <karthikthakkalapelly@gmail.com>
2026-07-12 23:54:18 +0530

    Added debug logs

--- src/llm.py
+++ src/llm.py
@@ -5,5 +5,17 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 
 def load_llm():
-    llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash",google_api_key=os.getenv("GOOGLE_API_KEY"),temperature=0)
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+    print("========== DEBUG ==========")
+    print("API key exists:", api_key is not None)
+    print("API key length:", len(api_key) if api_key else 0)
+    print("===========================")
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        google_api_key=api_key,
+        temperature=0
+    )
+
     return llm
